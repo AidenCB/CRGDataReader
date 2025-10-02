ee8662f9ae456ba0c610688465858ee5f8e4d9e2
Aiden Cabrera <35898714+AidenCB@users.noreply.github.com>
2025-10-02 16:18:49 -0400

    Update DataReaderWeb.py

--- DataReaderWeb.py
+++ DataReaderWeb.py
@@ -121,7 +121,7 @@ def cleanData(mainDf):
             converted = pd.to_numeric(df[column], errors='coerce')
             # Does NOT have NaN, convert to numeric
             nanCount = converted.isna().sum()
-            if not (nanCount == len(df) or nanCount > (len(df) * 0.8)):
+            if not (nanCount == len(df) or nanCount > (len(df) * 0.7)):
                 df[column] = converted 
 
 
