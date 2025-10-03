b1d4fd4b0a0b8cb0953c444e0e30cc333892529b
Aiden <doubleblade759@gmail.com>
2025-10-03 11:00:03 -0400

    Added Percentile

--- DataReaderWeb.py
+++ DataReaderWeb.py
@@ -102,7 +102,7 @@ def cleanData(mainDf):
     df = mainDf.copy()
 
     # Remove commas from numbers and replacing with periods
-    for column in df.columns:  # Only doing operation on strings
+    for column in df.select_dtypes(include="object").columns:  # Only doing operation on strings
         df[column] = df[column].str.replace(',', '.', regex=True) 
         # Check for "/", "-" 
         if df[column].str.contains(r'[\/\-]').any():
@@ -110,7 +110,7 @@ def cleanData(mainDf):
 
     # Convert strings to numbers
     # Do it FIRST because it might convert numbers to date incorrectly otherwise
-    for column in df.columns:
+    for column in df.select_dtypes(include="object").columns:
         converted = pd.to_numeric(df[column], errors='coerce')
         # Does NOT have NaN, convert to numeric
         if not (converted.isna().all()):
@@ -194,6 +194,7 @@ def findPercentiles(df, dfPercent, choice):
 
     # Convert to DataFrame: Count + list of values
     return pd.DataFrame(results).T  # transpose so columns are rows
+
 
 def showMathInfo(df):
     numericCols = df.select_dtypes(include=['number'])
